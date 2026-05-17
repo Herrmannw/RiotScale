7e93cc0e19ae0215cb49777ed60be4099fead98d
William Herrmann <william.a.herrmann@gmail.com>
2026-05-16 19:49:07 -0500

    changing CSS path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://herrmannw.github.io",
-  base: process.env.PUBLIC_BASE_PATH || "/RiotScale",
+  base: "/RiotScale",
 });
