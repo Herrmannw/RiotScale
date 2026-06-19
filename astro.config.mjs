9de62a6c9db67298511680bb613f26fad3fbf421
William Herrmann <william.a.herrmann@gmail.com>
2026-06-19 15:08:51 -0500

    Simplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://herrmannw.github.io",
   base: "/RiotScale",
-  integrations: [react()],
 });
