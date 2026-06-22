a72a5f511e5987bf274e91b6d9ecbb23adae080d
William Herrmann <william.a.herrmann@gmail.com>
2026-06-22 17:57:14 -0500

    Configure site for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://herrmannw.github.io",
-  base: "/RiotScale",
+  site: "https://riotscale.io",
+  base: "/",
 });
